d72b31e057022ff0b57a46defe9e20f983acd80b
Pierson Browne <pbrowne88@gmail.com>
2019-11-30 13:10:49 -0500

    added named tuple generator

--- nate/named_tuple_generator.py
+++ nate/named_tuple_generator.py
@@ -4,14 +4,37 @@ from typing import List, Tuple, NamedTuple
 
 # Define Named Tuple
 
-def define_named_tuple(observation_name:str, attribute_names:List[str]) -> NamedTuple:
+def define_named_tuple(observation_name, attribute_names:List[str]) -> NamedTuple:
     
     output_tuple:NamedTuple = namedtuple(observation_name, attribute_names)
 
     return output_tuple
 
 
+def create_observation_list(observation_name:str, **kwargs) -> List[NamedTuple]:
 
-
-
-
+    custom_named_tuple = define_named_tuple(observation_name, list(kwargs.keys()))
+    
+    #Length check: all of the lists fed in MUST be of the same length
+    
+    arg_lengths = [len(arg) for arg in kwargs.values()]
+    arg_length = set(arg_lengths)
+    
+    print(arg_length)
+    
+    if len(arg_length) != 1:
+        raise Exception("Not all of the input data is the same length.")
+        
+    observation_list = []
+    
+    for i in range(0, arg_length.pop()):
+        
+        variables = []
+        
+        for arg in kwargs:
+            variables.append(kwargs[arg][i])
+        
+        observation_list.append(custom_named_tuple(*variables))
+    
+    return observation_list
+    
