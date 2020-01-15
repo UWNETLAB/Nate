647a10349e9d8c5220b62a7a7b39dd0329aa1cca
Pierson Browne <pbrowne88@gmail.com>
2020-01-15 17:19:01 -0500

    updated importers for multi-file import

--- nate/import.py
+++ nate/import.py
@@ -1,22 +1,26 @@
 import pandas
-from typing import List
+from typing import List, Union
 from named_tuple_generator import create_observation_list
 from abc import ABC, abstractmethod
+
 
 class base_importer(ABC):
     @abstractmethod
     def __init__(self):
         self.raw_data = None
 
+    @abstractmethod
     def tupleize(self, tuple_name = "observation"):
-        kwarg_dict = {}
-        
-        keys = self.raw_data.columns.tolist()
-        
-        for i in range(0,len(keys)):
-            kwarg_dict[keys[i]] = list(self.raw_data[keys[i]])
-            
-        self.data = create_observation_list("observation", **kwarg_dict)
+        self.data = None
+
+    def list_texts(self):
+        return [i.text for i in self.data]
+
+    def list_times(self):
+        return [i.timestamp for i in self.data]
+
+    def list_ids(self):
+        return [i.unique_id for i in self.data]
 
 
 class import_dataframe(base_importer):
@@ -40,27 +44,44 @@ class import_dataframe(base_importer):
 
         self.process_dataframe(input_dataframe, text, unique_id,  timestamp, columns_to_keep)
 
+    def tupleize(self, series_dict, tuple_name = "obs"):
+        
+        kwarg_dict = {}
+        
+        keys = [i for i in series_dict.keys()]
+        
+        for i in range(0,len(keys)):
+            kwarg_dict[keys[i]] = list(series_dict[keys[i]])
+
+
+        self.data = create_observation_list("obs", **kwarg_dict)
+
     def process_dataframe(self, temp_data, text:str, unique_id:str = None,  timestamp:str = None, columns_to_keep:List = []):
         """
         This is a docstring
         """
-        series_list = []
+        
+
+        series_dict = {}
         special_column_list = [
             (text, "text"),
             (unique_id, "unique_id"),
             (timestamp, "timestamp")
         ]
 
+
         for special_column, special_column_name in special_column_list:
             if special_column != None:
                 temp_column = temp_data[special_column]
                 temp_column.name = special_column_name
-                series_list.append(temp_column)
+                series_dict[special_column_name] = temp_column
+
+
 
         for covariate_column in columns_to_keep:
-            series_list.append(temp_data[covariate_column])
+            series_dict[covariate_column.name] = covariate_column
 
-        self.raw_data = pandas.DataFrame(series_list).transpose()
+        self.tupleize(series_dict)
 
 
 class import_csv(import_dataframe, base_importer):
@@ -83,8 +104,22 @@ class import_csv(import_dataframe, base_importer):
     the data requires any kind of special treatment, prudent users will first load their data using pandas directly into python, and then use the 'import_dataframe'
     class to load their data into Nate.
     """
-    def __init__(self, file_path:str, text:str, unique_id:str = None,  timestamp:str = None, columns_to_keep:List = []):
-        temp_data:pandas.DataFrame = pandas.read_csv(file_path)
+    def __init__(self, file_paths:List, text:str, unique_id:str = None,  timestamp:str = None, columns_to_keep:List = []):
+        
+
+        if isinstance(file_paths, list):
+            df_list = []
+            for entry in file_paths:
+                df_list.append(pandas.read_csv(entry))
+            
+            temp_data = pandas.concat(df_list)
+
+        elif isinstance(file_paths, str):
+
+            temp_data = pandas.read_csv(file_paths)
+
+        else:
+            raise TypeError("file_paths must be either string or list of strings")
 
         self.process_dataframe(temp_data, text, unique_id,  timestamp, columns_to_keep)
 
@@ -110,10 +145,22 @@ class import_excel(import_dataframe, base_importer):
     class to load their data into Nate.
     """
 
-    def __init__(self, file_path:str, text:str, unique_id:str = None,  timestamp:str = None, columns_to_keep:List = []):
-
-        temp_data:pandas.DataFrame = pandas.read_excel(file_path)
-
-        self.process_dataframe(temp_data,  text, unique_id,  timestamp, columns_to_keep)
-
+    def __init__(self, file_paths:List, text:str, unique_id:str = None,  timestamp:str = None, columns_to_keep:List = []):
         
+
+        if isinstance(file_paths, list):
+            df_list = []
+            for entry in file_paths:
+                df_list.append(pandas.read_excel(entry))
+            
+            temp_data = pandas.concat(df_list)
+
+        elif isinstance(file_paths, str):
+
+            temp_data = pandas.read_excel(file_paths)
+
+        else:
+            raise TypeError("file_paths must be either string or list of strings")
+
+        self.process_dataframe(temp_data, text, unique_id,  timestamp, columns_to_keep)
+
