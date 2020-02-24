ba71c8ad465e52eea0d0202b2431827d5b5ade41
tcrick <43351367+tcrick@users.noreply.github.com>
2020-02-24 18:03:28 -0500

    new mp function to align with spacy serialization changes

--- nate/mp_suite/generic_mp.py
+++ nate/mp_suite/generic_mp.py
@@ -1,20 +1,19 @@
 from joblib import Parallel, delayed
-from toolz import partition_all
 from itertools import chain
-
+from functools import partial
+from spacy.util import minibatch
 
 def mp(items, function, cpu, *args):
     """
     This is a docstring.
     """
     batch_size = round(len(items)/cpu)
-    partitions = partition_all(batch_size, items)
-    temp = Parallel(n_jobs=cpu, max_nbytes=None)(delayed(function)(v, *args) for v in partitions)
-    if isinstance(temp[0], dict):
-        results = {}
-        for batch in temp:
-            for key, value in batch.items():
-                results.setdefault(key, []).extend(value)
-    elif isinstance(temp[0], (list, tuple)):
-        results = list(chain(*temp))
+    partitions = minibatch(items, size=batch_size)
+    executor = Parallel(n_jobs=cpu, backend="multiprocessing", prefer="processes")
+    do = delayed(partial(function, *args))
+    tasks = (do(batch) for batch in partitions)
+    results = executor(tasks)
+    results = list(itertools.chain(*temp))
+    
     return results
+
