1e4a2f54e34d31ffb000a8d1a19a6623fd9be079
Alex de Wit <alex@nafnlaus.ca>
2020-06-01 11:23:44 -0400

    Fix UTC error in timestamp_process

--- nate/importers/timestamp_process.py
+++ nate/importers/timestamp_process.py
@@ -5,17 +5,17 @@ from datetime import datetime, timezone
 
 
 def convert_times(times, timezone=timezone.utc):
-    """Convert all times to POSIX timestamps, in UTC."""
+    """Convert all times to POSIX timestamps."""
     timestamps = []
 
     for time in times:
         dt = datetime.strptime(time, "%m/%d/%Y %H:%M")
-        timestamps.append(int(dt.replace(tzinfo=timezone.utc).timestamp()))
+        timestamps.append(int(dt.replace(tzinfo=timezone).timestamp()))
 
     return timestamps
 
 
 def convert_time(time, timezone=timezone.utc):
-    """Convert a single time to POSIX timestamp, in UTC."""
+    """Convert a single time to POSIX timestamp."""
     dt = datetime.strptime(time, "%m/%d/%Y %H:%M")
-    return int(dt.replace(tzinfo=timezone.utc).timestamp())
+    return int(dt.replace(tzinfo=timezone).timestamp())
