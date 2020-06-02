c04aa4f86db86b7b0bb9fe75b5bba480fefca0ef
Alex de Wit <alex@nafnlaus.ca>
2020-06-02 15:39:29 -0400

    First pass on Cooc documentation

--- nate/cooc/cooc_class.py
+++ nate/cooc/cooc_class.py
@@ -1,5 +1,6 @@
-"""
-This is a MODULE docstring
+"""Definition of the `Cooc` pipeline, for co-occurence analysis.
+
+This module defines the `Cooc` pipeline, [description of cooc pipeline functionality].
 """
 from typing import Dict, Union, List
 from ..edgeburst.burst_mixin import BurstMixin
@@ -7,8 +8,19 @@ from nate.edgeburst.burst_class import Bursts
 
 
 class Cooc(BurstMixin):
-    """
-    This is a docstring
+    """The main object in the `Cooc` pipeline.
+
+    Attributes:
+        offset_dict (Dict): A dictionary with term-term pairs as keys and a list
+            of times when they occur as values.
+        lookup (Dict): A dictionary with the integer representation of a term as
+            key and the string representation as value.
+        minimum_offsets (int): The minimum number of 'offsets' - or occurrences
+            in the dataset - a given token/term pair must have had in order to
+            be retained.
+        from_svo (Bool): A flag to be passed to future steps in the pipeline
+            marking whether the data descended from an SVO class. 
+            [Should be removed on future development.]
     """
 
     def __init__(self,
@@ -29,7 +41,7 @@ class Cooc(BurstMixin):
                 in the `offset_dict` attribute.
         
         Returns:
-            A list of named tuples, each corresponding to one row in the dataset. 
+            List: A list of named tuples, each corresponding to one row in the dataset. 
         """
 
         if isinstance(index, slice) or isinstance(index, int):
@@ -39,14 +51,19 @@ class Cooc(BurstMixin):
 
 
     def cooc_to_burst(self, s=2, gamma=1):
-        """[summary]
+        """Returns an instance of the `Bursts` class.
         
         Args:
-            s (int, optional): [description]. Defaults to 2.
-            gamma (int, optional): [description]. Defaults to 1.
+            s (float, optional): s parameter for tuning Kleinberg algorithm.
+                Higher values make it more difficult for bursts to move up the
+                burst hierarchy. Defaults to 2.
+            gamma (float, optional): gamma parameter for tuning Kleinberg
+                algorithm. Higher values make it more difficult for activity to
+                be considered a burst. Defaults to 1.
         
         Returns:
-            [type]: [description]
+            Bursts: An instance of the `Bursts` class containing data from the
+                instance of the `Cooc` class it was called from.
         """
         offset_dict_strings, edge_burst_dict_strings, s, gamma, from_svo, lookup = self.burst_detection(
             s, gamma)
--- nate/cooc/cooc_offsets.py
+++ nate/cooc/cooc_offsets.py
@@ -1,6 +1,8 @@
+"""Builds the offset dictionary for the cooc pipeline.
+
+The dictionary is of the form {(word1, word2):[time1,time2,...], ...}.
 """
-This is a MODULE docstring
-"""
+
 import pandas as pd
 from time import time as marktime
 from typing import List
@@ -10,10 +12,23 @@ from collections import defaultdict
 
 
 def cooc_offsets(processed_list: List, time: List, minimum_offsets):
-    """
-    This is a docstring. Takes a list of documents and a list of their timestamps, returns a dictionary with term-pairs
-    as keys and a list of times when those term-pairs occur as values. Also returns a lookup dictionary, with integers
-    as keys for corresponding string values
+    """Generates the offset_dict for the `Cooc` pipeline.
+
+    Args:
+        processed_list (List): A list of lists, where each entry in the outer
+            list represents a text, and the entries of each inner list are 
+            the tokens found in those texts in string form.
+        time (List): A list of times for when each text was written.
+        minimum_offsets (int): The minimum number of 'offsets' - or occurrences
+            in the dataset - a given token/term pair must have in order to
+            be retained.
+
+    Returns:
+        Dict: The offset dictionary for the `Cooc` class, with word-word pairs
+            in integer format as keys and a list of offsets (occurence 
+            timestamps) as values.
+        Dict: A lookup dictionary for each word in the corpus, with the integer
+            representation as key and the string representation as value.
     """
     print("Generating Offsets:")
 
@@ -54,8 +69,23 @@ def cooc_offsets(processed_list: List, time: List, minimum_offsets):
 
 
 def text_to_int(processed_list):
-    """
-    This is a docstring. Converts a list of texts (which are lists of tokens) into integer representation
+    """Converts every word in a list of texts into an integer representation.
+
+    After conversion to the integer representation, the tokens of the text are
+    no longer in the same order. This function should only be used on texts
+    where the distance between tokens in the source text is not relevant. It
+    should only be used on texts where token co-occurence _in the same document_
+    is relevan
+    
+    Args:
+        processed_list (List): A list of texts, where each text is a
+            list of tokens (strings).
+
+    Returns:
+        List: A list of texts, where each text is a list of tokens (integers).
+        Dict: A lookup dict, to convert integer representations of tokens
+            to strings. It is of the form {i:s} where i is the integer
+            representation of the token, and s is the string representation.
     """
 
     # sort string tokens in each text, keeping only unique words
@@ -86,9 +116,18 @@ def text_to_int(processed_list):
 
 
 def cooc(time, word_ints):
-    """
-    This is a docstring. Takes a list of times and a list (document) of lists (word integers in each document). Returns 
-    dictionary with pairs of integers as keys and a list of timestamps (occurrences) as each value
+    """Generates co-occurence pairs from documents and their timestamps.
+
+    Args:
+        time (List): A list of of the times each text in word_ints was written.
+        word_ints (List): A list of lists, where each entry in the outer list
+            represents a text, and the entries of each inner list are the
+            integer representations of tokens found in those texts (as
+            produced by text_to_int).
+    
+    Returns:
+        Dict: A dictionary with token-token pairs as keys and a list of 
+            occurence timestamps as values.
     """
 
     # use defaultdict so that dictionary entries are created if they don't exist already
